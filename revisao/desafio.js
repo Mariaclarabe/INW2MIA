3044e4c560e379a2cfa4c72488bb7cb1e1a13e16
Logon Aluno <logonrmlocal@fiap.com.br>
2023-06-06 08:44:13 -0300

    revisao

---
+++ revisao/desafio.js
@@ -0,0 +1,7 @@
+const leia = require("prompt-sync")()
+
+let Nome = leia("Digite o seu nome : ")
+let temperaturaCelsius =parseFloat(leia("Digite a temperatura em celsius : "))
+
+console.log("oi "+nome+" a temperatura em graus Kelvin é "+(celsius+))
+
